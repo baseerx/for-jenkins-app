10eb2e46291743f78036feac29ad0372302ae380
baseerx <bsronnet@gmail.com>
2024-08-25 16:13:39 +0500

    Done with automation

--- src/App.js
+++ src/App.js
@@ -5,19 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React v3 - checking with pm2 and using .sh script
-
-        </a>
+       This is automation is done via jenkins by the Full Stack | DevOps engineer Baseer Khan
       </header>
     </div>
   );
